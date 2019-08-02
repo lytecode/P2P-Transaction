715ec4dd1cfcd87a4ef5bf4f659d6ea3139fae6f
Basil <mbonubasil@gmail.com>
2019-08-02 15:31:39 +0100

    Add JWT credentials

--- server/config/index.js
+++ server/config/index.js
@@ -4,7 +4,9 @@ const config = {
   MONGOURI: process.env.MONGOURI,
   PORT: process.env.PORT,
   SENDGRID_USER: process.env.SENDGRID_USER,
-  SENDGRID_PASSWORD: process.env.SENDGRID_PASSWORD
+  SENDGRID_PASSWORD: process.env.SENDGRID_PASSWORD,
+  JWT_SECRET: process.env.JWT_SECRET,
+  TOKEN_EXPIRESIN: process.env.TOKEN_EXPIRESIN
 };
 
 module.exports = config;
