de79a23cd679c59826267c4d13fc58f8d5449fe7
Basil <mbonubasil@gmail.com>
2019-08-03 04:00:00 +0100

    Remove verify field

--- server/models/user.js
+++ server/models/user.js
@@ -9,11 +9,6 @@ const userSchema = new Schema(
     email: { type: String, required: true, trim: true, unique: true },
     phoneNumber: { type: String, required: true, unique: true, trim: true },
     balance: { type: Number, default: 0.0 },
-    verified: {
-      type: String,
-      default: "false",
-      enum: ["false", "verified", "pending"]
-    },
     otp: { type: String, default: "" },
     pin: { type: String, required: true, trim: true }
   },
